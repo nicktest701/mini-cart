34a66b363498332b644673e445f9fae71a132e62
nicktest701 <nicktest701@gmail.com>
2023-10-02 23:07:54 +0000

    init

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(cors({}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, 'uploads')));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 //routes
 app.use('/user', userRoute);
@@ -25,9 +26,9 @@ app.use('/supplier', supplierRoute);
 app.use('/customer', customerRoute);
 app.use('/cart', cartRoute);
 
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 //error handlers
 app.use((req, res, next) => {
